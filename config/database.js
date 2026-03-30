1233f40ecfaeff25fd58e285183d02a79f08903c
Bennett <bennettdishman2@gmail.com>
2026-03-29 22:33:14 -0500

    Add connection timeout to prevent server hangs on DB issues

--- config/database.js
+++ config/database.js
@@ -10,6 +10,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: isProduction ? { rejectUnauthorized: false } : false,
+  connectionTimeoutMillis: 10000,
 });
 
 export default pool;
